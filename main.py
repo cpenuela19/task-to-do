eb059d4a1aa43d75d052890dcc41fbd978735c31
cpenuela19 <ce.penuela20@uniandes.edu.co>
2025-01-25 20:25:14 -0500

    base schema

--- main.py
+++ main.py
@@ -58,6 +58,6 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print('Database created?')
+        print('Data base created tester')
     app.run(debug=True)
 
